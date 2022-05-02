ba891716f3bcf3693a74de2738bb8b5241a89132
Ilia Krets <mrchair2170@gmail.com>
2022-05-02 17:16:01 +0200

    Implement multi-head attention

--- nlp_exercises/layers/attention.py
+++ nlp_exercises/layers/attention.py
@@ -2,18 +2,26 @@ from flax import linen as nn
 from typing import Callable, Optional
 import jax.numpy as jnp
 
-class Attention(nn.Module):
+
+class MultiHeadAttention(nn.Module):
     dimension: int
+    num_heads: int = 1
     mask_queries_from: Optional[int] = None
 
     WK_init: Callable = nn.initializers.lecun_normal()
     WQ_init: Callable = nn.initializers.lecun_normal()
     WV_init: Callable = nn.initializers.lecun_normal()
 
+    def _split_heads(self, x):
+        split = x.reshape((-1, self.num_heads, self.dimension // self.num_heads))
+        reorder = split.transpose((1, 0, 2))
+        return reorder
+
     @nn.compact
     def __call__(self, key_x, query_x, value_x):
         assert key_x.shape == query_x.shape
         assert query_x.shape == value_x.shape
+        assert self.dimension % self.num_heads == 0
 
         WX_shape = (key_x.shape[-1], self.dimension)
         WK = self.param("WK", self.WK_init, WX_shape)
@@ -21,21 +29,29 @@ class Attention(nn.Module):
         WV = self.param("WV", self.WV_init, WX_shape)
 
         if self.mask_queries_from is None:
-            mask_from_concrete = key_x.shape[-1]
+            mask_from_concrete = key_x.shape[0]
         else:
-            assert self.mask_queries_from <= key_x.shape[-1]
+            assert self.mask_queries_from <= key_x.shape[0]
             mask_from_concrete = self.mask_queries_from
 
-        mask_zeros = jnp.zeros((1, mask_from_concrete))
-        mask_neginf = jnp.full((1, key_x.shape[-1] - mask_from_concrete), -jnp.inf)
-        mask = jnp.concatenate([mask_zeros, mask_neginf], axis=1)
+        mask_zeros = jnp.zeros((1, 1, mask_from_concrete))
+        mask_neginf = jnp.full((1, 1, key_x.shape[0] - mask_from_concrete), -jnp.inf)
+        mask = jnp.concatenate([mask_zeros, mask_neginf], axis=-1)
 
         K = jnp.dot(key_x, WK)
         Q = jnp.dot(query_x, WQ)
         V = jnp.dot(value_x, WV)
 
-        logits = jnp.matmul(Q, K.T) / jnp.sqrt(self.dimension)
-        weights = nn.softmax(logits + mask)
-        return jnp.matmul(weights, V)
+        K_heads = self._split_heads(K)
+        Q_heads = self._split_heads(Q)
+        V_heads = self._split_heads(V)
 
-        
+        head_logits = (
+            jnp.matmul(Q_heads, K_heads.transpose((0, 2, 1)))
+            / jnp.sqrt(self.dimension // self.num_heads)
+            + mask
+        )
+
+        head_weights = nn.softmax(head_logits)
+        head_result = jnp.matmul(head_weights, V_heads)
+        return head_result.transpose((1, 0, 2)).reshape((-1, self.dimension))
--- nlp_exercises/layers/test_attention.py
+++ nlp_exercises/layers/test_attention.py
@@ -1,19 +1,22 @@
-from layers.attention import Attention
+from layers.attention import MultiHeadAttention
 import numpy as np
 from jax import random
 from jax import numpy as jnp
 from flax import linen as nn
 
-def test_unmasked_attention_trivial():
+
+def test_unmasked_attention_one_head():
     keys_x = np.array([[1, 2], [-5, -10]])
     queries_x = np.array([[3, 4], [5, 6]])
     values_x = np.array([[7, 8], [9, 10]])
 
-    attention = Attention(dimension=1, 
-                          WK_init=lambda _, __: np.array([[1], [0]]),
-                          WQ_init=lambda _, __: np.array([[0], [1]]),
-                          WV_init=lambda _, __: np.array([[1], [1]]))
-
+    attention = MultiHeadAttention(
+        dimension=1,
+        num_heads=1,
+        WK_init=lambda _, __: np.array([[1], [0]]),
+        WQ_init=lambda _, __: np.array([[0], [1]]),
+        WV_init=lambda _, __: np.array([[1], [1]]),
+    )
 
     random_key = random.PRNGKey(0)
     params = attention.init(random_key, keys_x, queries_x, values_x)
@@ -27,16 +30,19 @@ def test_unmasked_attention_trivial():
     assert weights1.dot(V) == result[0]
     assert weights2.dot(V) == result[1]
 
-def test_masked_attention_trivial():
+
+def test_masked_attention_one_head():
     keys_x = np.array([[1, 2], [-5, -10]])
     queries_x = np.array([[3, 4], [5, 6]])
     values_x = np.array([[7, 8], [9, 10]])
 
-    attention = Attention(dimension=1, mask_queries_from=1, 
-                          WK_init=lambda _, __: np.array([[1], [0]]),
-                          WQ_init=lambda _, __: np.array([[0], [1]]),
-                          WV_init=lambda _, __: np.array([[1], [1]]))
-
+    attention = MultiHeadAttention(
+        dimension=1,
+        mask_queries_from=1,
+        WK_init=lambda _, __: np.array([[1], [0]]),
+        WQ_init=lambda _, __: np.array([[0], [1]]),
+        WV_init=lambda _, __: np.array([[1], [1]]),
+    )
 
     random_key = random.PRNGKey(0)
     params = attention.init(random_key, keys_x, queries_x, values_x)
@@ -49,4 +55,24 @@ def test_masked_attention_trivial():
     V = np.array([[15], [19]])
     assert weights1.dot(V) == result[0]
     assert weights2.dot(V) == result[1]
-    
+
+
+def test_unmasked_attention_multiple_heads():
+    x = np.array([[1, 2], [-5, -10], [-2, -4]])
+
+    W_init = lambda _, __: np.array([[1, 0, 1, -1], [0, 1, 1, -1]])
+
+    attention = MultiHeadAttention(
+        dimension=4, num_heads=4, WK_init=W_init, WQ_init=W_init, WV_init=W_init
+    )
+
+    random_key = random.PRNGKey(0)
+    params = attention.init(random_key, x, x, x)
+    result = attention.apply(params, x, x, x)
+
+    V = np.array([[1, 2, 3, -3], [-5, -10, -15, 15], [-2, -4, -6, 6]])
+
+    for i in range(4):
+        QKTi = jnp.matmul(V[:, i : i + 1], (V[:, i : i + 1].T))
+        weightsi = nn.softmax(QKTi)
+        assert jnp.array_equal(weightsi.dot(V[:, i]), result[:, i])
