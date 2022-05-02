Fix masking

nlp_exercises/layers/attention.py
@@ -6,7 +6,7 @@ import jax.numpy as jnp
 class MultiHeadAttention(nn.Module):
     dimension: int
     num_heads: int = 1
-    mask_queries_from: Optional[int] = None
+    masked: bool = False
 
     WK_init: Callable = nn.initializers.lecun_normal()
     WQ_init: Callable = nn.initializers.lecun_normal()
@@ -28,15 +28,12 @@ class MultiHeadAttention(nn.Module):
         WQ = self.param("WQ", self.WQ_init, WX_shape)
         WV = self.param("WV", self.WV_init, WX_shape)
 
-        if self.mask_queries_from is None:
-            mask_from_concrete = key_x.shape[0]
+        if not self.masked:
+            mask = jnp.zeros((1, 1, 1))
         else:
-            assert self.mask_queries_from <= key_x.shape[0]
-            mask_from_concrete = self.mask_queries_from
-
-        mask_zeros = jnp.zeros((1, 1, mask_from_concrete))
-        mask_neginf = jnp.full((1, 1, key_x.shape[0] - mask_from_concrete), -jnp.inf)
-        mask = jnp.concatenate([mask_zeros, mask_neginf], axis=-1)
+            mask = jnp.triu(
+                jnp.full((1, key_x.shape[-2], key_x.shape[-2]), -jnp.inf), k=1
+            )
 
         K = jnp.dot(key_x, WK)
         Q = jnp.dot(query_x, WQ)

nlp_exercises/layers/test_attention.py
@@ -38,7 +38,7 @@ def test_masked_attention_one_head():
 
     attention = MultiHeadAttention(
         dimension=1,
-        mask_queries_from=1,
+        masked=True,
         WK_init=lambda _, __: np.array([[1], [0]]),
         WQ_init=lambda _, __: np.array([[0], [1]]),
         WV_init=lambda _, __: np.array([[1], [1]]),
@@ -48,7 +48,7 @@ def test_masked_attention_one_head():
     params = attention.init(random_key, keys_x, queries_x, values_x)
     result = attention.apply(params, keys_x, queries_x, values_x)
 
-    QKT = np.array([[4, -np.inf], [6, -np.inf]])
+    QKT = np.array([[4, -np.inf], [6, -30]])
     weights1 = nn.softmax(QKT[0])
     weights2 = nn.softmax(QKT[1])
 